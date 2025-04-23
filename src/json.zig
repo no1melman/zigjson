87eef12a2efc362272ff96638338dae89eb2faae
Callum Linington <callum.linington@live.co.uk>
2025-04-24 00:16:22 +0100

    feat: found the structure

--- src/json.zig
+++ src/json.zig
@@ -14,30 +14,31 @@ const quote = '"';
 const ReadResultTag = enum { ok, need_more };
 const ReadResult = union(ReadResultTag) { ok: u8, need_more: void };
 
-const ReaderPosition = struct {
-    index: usize
-};
+const ReaderPosition = struct { index: usize = 0 };
+
 const Utf8JsonReader = struct {
     position: ReaderPosition,
-    buffer: []const u8
+    buffer: *const []u8,
+    tokenType: JsonTokenType = .none,
+
+    pub fn read_next(self: *Utf8JsonReader) bool {
+        if (self.position.index < self.buffer.len) {
+            print("read next: {c}\n", .{self.buffer.*[self.position.index]});
+
+            // need to discern the actual token read...
+            self.tokenType = .start_object;
+
+            self.position.index += 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
 
-pub fn create_reader(buffer: []const u8) !Utf8JsonReader {
-    const position = ReaderPosition{.index = 0};
-    const reader = Utf8JsonReader{.position = position, .buffer = buffer };
+pub fn create_reader(buffer: *const []u8) !Utf8JsonReader {
+    const position = ReaderPosition{ .index = 0 };
+    const reader = Utf8JsonReader{ .position = position, .buffer = buffer };
 
     return reader;
 }
-
-pub fn read_next(buffer: []const u8) !ReadResult {
-    var index: usize = 0;
-    while (index < buffer.len) {
-        print("read next: {c}\n", .{buffer[index]});
-        index += 1;
-    }
-
-    print("leaving...\n", .{});
-
-    return ReadResult{ .ok = 'y' };
-}
-
--- src/main.zig
+++ src/main.zig
@@ -4,35 +4,31 @@ const File = std.fs.File;
 const json = @import("json.zig");
 
 pub fn main() !void {
-    print("hello there", .{});
+    print("hello there\n", .{});
 
     const file = try std.fs.cwd().openFile("compile_commands.json", .{});
     defer file.close();
 
     var buffer: [30]u8 = [_]u8{0} ** 30;
     var read_bytes: usize = undefined;
-    var should_continue: bool = true;
 
-
-    while (read_bytes > 0 and should_continue) {
+    while (read_bytes > 0) {
         read_bytes = try file.read(&buffer);
-
-        const utf8_reader = try json.create_reader(&buffer);
-        _ = utf8_reader.position.index;
 
         if (read_bytes == 0) {
             break;
         }
 
         const buffer_slice = buffer[0..read_bytes];
-        const result = try json.read_next(buffer_slice);
+        var utf8_reader = try json.create_reader(&buffer_slice);
+        while (utf8_reader.read_next()) {
+            const readToken = utf8_reader.tokenType;
 
-        should_continue = switch (result) {
-            .ok => false,
-            .need_more => true
-        };
+            print("Read: {}\n", .{readToken});
 
-        print("Buffer: {s} ({})\n", .{ buffer[0..read_bytes], read_bytes });
+            break;
+        }
+        break;
     }
 
     print("Done\n", .{});
